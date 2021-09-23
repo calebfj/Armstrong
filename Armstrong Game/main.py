698beca46c28d59da06534f5ba32b813434aad16
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-09-22 20:38:50 -0400

    choices added

--- Armstrong Game/main.py
+++ Armstrong Game/main.py
@@ -3,4 +3,22 @@ from tkinter import *
 root = Tk()
 root.title("Game GUI")
 
+def choice1():
+    return
+
+def choice2():
+    return
+
+def choice3():
+    return
+
+button_choice1 = Button(root, text="Choice 1", padx=40, pady=20, command=choice1)
+button_choice1.grid(row=3, column=0)
+
+button_choice2 = Button(root, text="Choice 2", padx=40, pady=20, command=choice3)
+button_choice2.grid(row=3, column=1)
+
+button_choice3 = Button(root, text="Choice 3", padx=40, pady=20, command=choice3)
+button_choice3.grid(row=3, column=2)
+
 root.mainloop()
