1d762d592ae974bc1f4101108e91153b84b8fbb3
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-09-22 21:28:21 -0400

    maybe

--- Armstrong Game/main.py
+++ Armstrong Game/main.py
@@ -3,22 +3,31 @@ from tkinter import *
 root = Tk()
 root.title("Game GUI")
 
+myLabel = Label(root, text = "")
+myLabel.grid(row=0, column=0)
+
 def choice1():
-    return
+    msg = "You picked choice 1!"
+    myLabel = Label(root, text=msg)
+    myLabel.grid(row=0, column=0)
 
 def choice2():
-    return
+    msg = "You picked choice 2!"
+    myLabel = Label(root, text=msg)
+    myLabel.grid(row=0, column=0)
 
 def choice3():
-    return
+    msg = "You picked choice 3!"
+    myLabel = Label(root, text=msg)
+    myLabel.grid(row=0, column=0)
 
 button_choice1 = Button(root, text="Choice 1", padx=40, pady=20, command=choice1)
-button_choice1.grid(row=3, column=0)
+button_choice1.grid(row=1, column=0)
 
-button_choice2 = Button(root, text="Choice 2", padx=40, pady=20, command=choice3)
-button_choice2.grid(row=3, column=1)
+button_choice2 = Button(root, text="Choice 2", padx=40, pady=20, command=choice2)
+button_choice2.grid(row=1, column=1)
 
 button_choice3 = Button(root, text="Choice 3", padx=40, pady=20, command=choice3)
-button_choice3.grid(row=3, column=2)
+button_choice3.grid(row=1, column=2)
 
 root.mainloop()
