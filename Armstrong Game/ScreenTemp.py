a4030589204973444d5ee09124343ff97c098a26
TallonMcAbee <tallon.mcabee@gmail.com>
2021-12-02 10:40:20 -0500

    Added MORE comments

--- Armstrong Game/ScreenTemp.py
+++ Armstrong Game/ScreenTemp.py
@@ -72,6 +72,8 @@ def congrats(t):
     return t
 
 def main():
+    # Since we utilize a ton of methods to create our useable game environment, globals are utilizied extensively to allow for references to the same variable across methods and
+    # classes. Although, if we had more time we would move a lot of these variables into their own classes to help clean up the code.
     global yellowIsDisabled
     global redIsDisabled
     global greenIsDisabled
@@ -79,6 +81,9 @@ def main():
     global smallIsDisabled
     global m
 
+    # These methods check if the game is in its prestart gamestate (99), which is basically just the start menu for the game. If it detects that the gamestate is still at its
+    # default 99 value, then it will disable buttons so that no conflicts can occur if a player clicks any other button besides the start button. So when in gamestate 99 the
+    # pause button is the only activated button and in any other situation all other buttons would be activated.
     if gameStateNum == 99:
         yellowIsDisabled = True
         redIsDisabled = True
@@ -92,16 +97,23 @@ def main():
         blueIsDisabled = False
         smallIsDisabled = False
 
+    # These 3 statements setup the baseline portion of our GUI, with the root object acting as the reference for our GUI as a whole. Any changes to the GUI will call on
+    # this root object in order to change or update information since anything outside of this object will be ignored by the Tkinter library while running the GUI's
+    # thread. In this case, we set the root object to a default 1920x1080 screen resolution and have it set to scale with the size of any monitor or output device it runs on.
     root = tk.Tk()
     root.geometry("1920x1080")
     root.tk.call('tk', 'scaling', 2.0)
 
+    # These statements are the bread and butter to our threading management outside of the Tkinter library. In this case we are utilizing the Python library,
+    # in order to create a thread que where a thread is initialized utilizing the callback congrats method and will be storing the "Congrats!" string data. After the thread
+    # has ran, its data is grabbed from the que and stored within global variable m.
     que = Queue()
     t = Thread(target=lambda c, arg1: c.put(congrats(arg1)),args=(que,"Congrats!"))
     t.start()
     t.join()
     m = que.get()
 
+    # This enables the LCD screen upon main method initialization
     lcd.backlight_enabled
 
     # GameState methods are essentially containers for each game scenario. Using this list,
@@ -151,7 +163,7 @@ def main():
         GameState(  # finale
             "",
 
-            "Restart game",
+            "Restart game", # Restart game option
             "",
             "",
             ""
@@ -188,6 +200,9 @@ def main():
         if not (gameStateNum == 5 or PlayerValues.isDead() or smallIsDisabled):
             root.event_generate('<<small>>', when='tail')
 
+    # This method is the handler utilized to light up the on board LCD bulbs based on numberOfCollected's value. Although logically, a player should not be able to collect
+    # more then 4 collectibles at a time, the method still checks for a max limit of 4 when being utilized. This is done in order to stop the program from hitting a null pointer error
+    # when it tries to light up a 5th bulb that does not exist. 
     def checkLights(numberOfCollected):
         for x in range(0, numberOfCollected):
             if x == 4:
@@ -231,7 +246,9 @@ def main():
             runningClock.config(text=f"Current Run Time: {MinuteTime:.0f} minutes {SecondTime:.0f} seconds", bg="#f0f0f0")
             runningClock.after(225, tick)
 
-    # This method activates when the player activates the pause_button
+    # This method activates when the player activates the pause_button and takes in no parameters. This function primarily handles
+    # updating game values when the game is starting for the first time (Since start menu utilizes the same button as the pause menu)
+    # and updating key GUI centric varibles such as boolean variables concerning which buttons are disabled from use and in-game time.
     def pause():
         global paused
         global pausedTime
@@ -278,6 +295,8 @@ def main():
             paused = False
             pausedTime += time.time()
 
+    # This is a passthrough method used to call upon the pause function when a keydown is detected. It was primarily used during debugging and development
+    # on windows, however not it serves no functionality when paired with the controller.
     def keydown(e):
         pause()
 
@@ -287,8 +306,10 @@ def main():
     def updateGameValues():
         global collectiblesList, numberOfCollected, pausedTime, yellowIsDisabled, redIsDisabled, greenIsDisabled, blueIsDisabled, smallIsDisabled, m
 
+        # Current squad value that is utilized at the end of the game to notify you if you saved your squad or not
         squad = "You did not save your squad!"
 
+        # This is a running total value that tracks how many collectibles the player has and is recalculated everytime a updateGameValues method is called.
         numberOfCollected = 0
 
         if PlayerValues.hasBandages():
